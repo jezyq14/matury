130a7d616b286d35ee315fa5438e26256830a25f
jezyq14 <michal.muzyka@proton.me>
2025-07-10 17:15:28 +0200

    inf2024 zad 1.2

---
+++ inf2024/zad1.2.py
@@ -0,0 +1,37 @@
+def alg(n, m, board):
+	P = [[False for _ in range(m)] for _ in range(n)]
+
+	P[0][0] = True
+
+	for i in range(n):
+		for j in range(m):
+			if board[i][j] == 0:
+				P[i][j] = False
+			else:
+				if i == 0 and j != 0:
+					P[i][j] = P[i][j - 1]
+				elif i != 0 and j == 0:
+					P[i][j] = P[i - 1][j]
+				elif i != 0 and j != 0:
+					P[i][j] = P[i][j - 1] or P[i - 1][j]
+
+	return P[n-1][m-1]
+
+A = [
+	[1,0,1,1,1],
+	[0,1,1,1,1],
+	[1,1,1,1,1],
+    [1,1,1,1,1],
+    [1,1,1,1,1]
+]
+
+print(alg(5,5,A))
+
+B = [
+	[1,0,0,0],
+	[1,0,0,0],
+    [1,0,0,0],
+    [1,1,1,1]
+]
+
+print(alg(4,4,B))
